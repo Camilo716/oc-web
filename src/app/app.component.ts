9a03f15a02f780299ed1ca589b63d3ce4bfea9a6
camilogs <camilo.gonzalez.col@gmail.com>
2024-02-25 17:00:38 -0500

    Add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeroComponent } from "./hero/hero.component";
+import { HeroComponent } from './hero/hero.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeroComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, HeroComponent, NavbarComponent],
 })
 export class AppComponent {
   title = 'oc-web';
