ca78c0f28912880e176449a7ec45d0b0314d5f5e
camilogs <camilo.gonzalez.col@gmail.com>
2024-02-27 17:41:31 -0500

    Add sample footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ModulesComponent } from './modules/modules.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HeroComponent, NavbarComponent, ModulesComponent],
+  imports: [
+    RouterOutlet,
+    HeroComponent,
+    NavbarComponent,
+    ModulesComponent,
+    FooterComponent,
+  ],
 })
 export class AppComponent {
   title = 'oc-web';
