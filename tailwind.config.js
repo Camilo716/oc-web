ab24e2769cfa6d7d18c65db17275cbf0630bce67
camilogs <camilo.gonzalez.col@gmail.com>
2024-02-27 19:40:58 -0500

    Se agregan colores corporativos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ocGray: "#606060",
+        ocBlue: "#0478a5",
+      },
+    },
   },
   plugins: [],
-}
-
+};
